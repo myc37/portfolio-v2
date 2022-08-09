075c6f96d18ced1a2d7e72454797fbd0a4636909
myc37 <nvjn37@gmail.com>
2022-08-09 18:52:13 +0800

    chore: add constants directory

---
+++ constants/enums.ts
@@ -0,0 +1,33 @@
+export enum SOCIAL {
+	GITHUB = "Github",
+	LINKEDIN = "Linkedin",
+	TELEGRAM = "Telegram",
+	RESUME = "Resume",
+}
+
+export enum SECTION {
+	HOME = "Home",
+	ABOUT = "About",
+	WORK = "Work",
+	PROJECTS = "Projects",
+	CONTACT = "Contact",
+}
+
+export enum THEME {
+	DARK = "Dark",
+	LIGHT = "Light",
+}
+
+export enum TECH {
+	ALGOLIA = "Algolia",
+	CHAKRA = "Chakra",
+	EXPRESS = "Express",
+	FIREBASE = "Firebase",
+	MATERIAL = "Material",
+	NEXTJS = "Next.js",
+	POSTGRESQL = "Postgresql",
+	PRISMA = "Prisma",
+	REACT = "React",
+	TAILWIND = "Tailwind",
+	WEBSITE = "Website",
+}
