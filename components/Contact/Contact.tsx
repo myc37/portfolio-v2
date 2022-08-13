7d54cdd7e652c7a6b1f90fbffb22c30645a7cf38
myc37 <nvjn37@gmail.com>
2022-08-14 03:20:25 +0800

    feat: add contact page

---
+++ components/Contact/Contact.tsx
@@ -0,0 +1,86 @@
+import HoverableIcon from "components/HoverableIcon";
+import { SECTION, SIZE, SOCIAL } from "constants/enums";
+import React, { useEffect, useState } from "react";
+import contactStyles from "./Contact.module.scss";
+
+const Contact = () => {
+	const [alert, setAlert] = useState("");
+
+	const handleCopyToClipboard = (value: string) => {
+		navigator.clipboard.writeText(value);
+		setAlert(value);
+	};
+
+	useEffect(() => {
+		if (alert) {
+			const newAlert = document.createElement("div");
+			newAlert.innerText = `copied to clipboard!`;
+			newAlert.classList.add("custom-alert");
+			const contactContainer =
+				document.getElementById("contact-container");
+			contactContainer?.appendChild(newAlert);
+
+			setTimeout(() => {
+				newAlert.remove();
+			}, 3000);
+		}
+	}, [alert]);
+
+	return (
+		<div id="contact-container" className={contactStyles.container}>
+			<span className="header">
+				<HoverableIcon
+					name={SECTION.CONTACT}
+					size={SIZE.LARGE}
+					active
+					hideLabel
+					disableFloat
+				/>
+				<h1>Get In Touch!</h1>
+			</span>
+			<div className={contactStyles.contactContainer}>
+				<span>
+					<p>
+						I am always looking for new opportunities and/or to
+						simply expand my network!
+					</p>
+					<p>Feel free to contact me at any of these channels :)</p>
+				</span>
+				<div className={contactStyles.socialContainer}>
+					{contactInfos.map(({ name, value }) => (
+						<div
+							key={name}
+							className={contactStyles.social}
+							onClick={() => handleCopyToClipboard(value)}
+						>
+							<HoverableIcon active disableFloat name={name} />
+							<h3>{value}</h3>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+type ContactInfo = {
+	name: SOCIAL;
+	value: string;
+};
+
+const contactInfos: ContactInfo[] = [
+	{
+		name: SOCIAL.EMAIL,
+		value: "nvjn37@gmail.com",
+	},
+	{
+		name: SOCIAL.TELEGRAM,
+		value: "@myc37",
+	},
+	{
+		name: SOCIAL.LINKEDIN,
+		value: "N Vijay Narayanan",
+	},
+];
+
+export default Contact;
