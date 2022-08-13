0be1284e1cf436d18632c41fbca49a524232471d
myc37 <nvjn37@gmail.com>
2022-08-14 03:21:41 +0800

    feat: update navbar to be dynamic

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,10 +1,50 @@
 import HoverableIcon from "components/HoverableIcon";
-import { SECTION, SOCIAL } from "constants/enums";
-import React, { useState } from "react";
+import { SECTION, SIZE, SOCIAL } from "constants/enums";
+import React, { FC, RefObject, useEffect, useState } from "react";
 import navStyles from "./Navbar.module.scss";
 
-const Navbar = () => {
+type Props = {
+	sectionsInfo: Record<SECTION, RefObject<HTMLDivElement>>;
+};
+
+const Navbar: FC<Props> = ({ sectionsInfo }) => {
 	const [activeSectionId, setActiveSectionId] = useState(0);
+
+	useEffect(() => {
+		const homeSection = sectionsInfo.Home.current;
+		const aboutSection = sectionsInfo.About.current;
+		const workSection = sectionsInfo.Work.current;
+		const projectsSection = sectionsInfo.Projects.current;
+		const contactSection = sectionsInfo.Contact.current;
+
+		if (
+			!homeSection ||
+			!aboutSection ||
+			!workSection ||
+			!projectsSection ||
+			!contactSection
+		)
+			return;
+
+		window.onscroll = function () {
+			if (isInView(contactSection)) {
+				setActiveSectionId(4);
+			} else if (isInView(projectsSection)) {
+				setActiveSectionId(3);
+			} else if (isInView(workSection)) {
+				setActiveSectionId(2);
+			} else if (isInView(aboutSection)) {
+				setActiveSectionId(1);
+			} else {
+				setActiveSectionId(0);
+			}
+		};
+	}, [sectionsInfo]);
+
+	const handleScroll = (ref: RefObject<HTMLDivElement>) => {
+		ref.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+	};
+
 	return (
 		<nav className={navStyles.container}>
 			<div className={navStyles.tabs}>
@@ -12,19 +52,29 @@ const Navbar = () => {
 					<HoverableIcon
 						key={section}
 						name={section}
-						large
+						size={SIZE.LARGE}
 						active={index === activeSectionId}
-						onClick={() => setActiveSectionId(index)}
+						onClick={() => handleScroll(sectionsInfo[section])}
 					/>
 				))}
 			</div>
 			<div className={navStyles.socials}>
-				{Object.values(SOCIAL).map((social) => (
-					<HoverableIcon key={social} name={social} />
-				))}
+				{Object.values(SOCIAL)
+					.filter((social) => social !== SOCIAL.EMAIL)
+					.map((social) => (
+						<HoverableIcon
+							key={social}
+							name={social}
+							size={SIZE.MEDIUM}
+						/>
+					))}
 			</div>
 		</nav>
 	);
+};
+
+const isInView = (section: HTMLDivElement) => {
+	return section.getBoundingClientRect().top <= window.innerHeight / 2;
 };
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
+import About from "components/About";
+import Contact from "components/Contact";
+import Hero from "components/Hero";
 import Navbar from "components/Navbar";
 import Projects from "components/Projects";
 import Work from "components/Work";
+import { SECTION } from "constants/enums";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { RefObject, useRef } from "react";
 
 const Home: NextPage = () => {
+	const homeRef = useRef<HTMLDivElement>(null);
+	const aboutRef = useRef<HTMLDivElement>(null);
+	const workRef = useRef<HTMLDivElement>(null);
+	const projectsRef = useRef<HTMLDivElement>(null);
+	const contactRef = useRef<HTMLDivElement>(null);
+
+	const sectionsInfo: Record<SECTION, RefObject<HTMLDivElement>> = {
+		Home: homeRef,
+		About: aboutRef,
+		Work: workRef,
+		Projects: projectsRef,
+		Contact: contactRef,
+	};
+
 	return (
 		<div>
 			<Head>
@@ -13,10 +32,23 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
-				<Navbar />
+				<Navbar sectionsInfo={sectionsInfo} />
 				<div className="sections">
-					<Projects />
-					<Work />
+					<div ref={sectionsInfo.Home}>
+						<Hero aboutRef={sectionsInfo.About} />
+					</div>
+					<div ref={sectionsInfo.About}>
+						<About />
+					</div>
+					<div ref={sectionsInfo.Work}>
+						<Work />
+					</div>
+					<div ref={sectionsInfo.Projects}>
+						<Projects />
+					</div>
+					<div ref={sectionsInfo.Contact}>
+						<Contact />
+					</div>
 				</div>
 			</main>
 		</div>
