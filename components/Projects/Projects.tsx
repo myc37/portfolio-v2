2219f30e1510ae5920f31866df8f3c0a7bb1853a
myc37 <nvjn37@gmail.com>
2022-08-15 19:57:00 +0800

    feat: update projects page to be responsive

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,7 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import FadeIn from "components/FadeIn";
 import HoverableIcon from "components/HoverableIcon";
-import { OTHER, SECTION, SIZE, SOCIAL, TECH } from "constants/enums";
+import SectionHeader from "components/SectionHeader";
+import { OTHER, SECTION, SIZE, SOCIAL, TECH } from "lib/enums";
+import Image from "next/image";
 import { FC } from "react";
 import projectStyles from "./Projects.module.scss";
 
@@ -9,16 +10,7 @@ const Projects = () => {
 	return (
 		<div className={projectStyles.container}>
 			<FadeIn>
-				<span className="header">
-					<HoverableIcon
-						name={SECTION.PROJECTS}
-						size={SIZE.LARGE}
-						active
-						hideLabel
-						disableFloat
-					/>
-					<h1>Personal Projects</h1>
-				</span>
+				<SectionHeader section={SECTION.PROJECTS} />
 				<div className={projectStyles.projectsContainer}>
 					{projects.map((project, index) => (
 						<ProjectCard
@@ -37,6 +29,7 @@ type Project = {
 	name: string;
 	blurb: string;
 	url: string;
+	githubUrl: string;
 	stack: TECH[];
 };
 
@@ -45,24 +38,28 @@ const projects: Project[] = [
 		name: "WookieLeaks",
 		blurb: "WookieLeaks is a Star Wars-themed web page that contains all kinds of information about characters from the hit series. It even has a quiz for you to test how well you know your star wars characters!",
 		url: "https://wookie-leaks.vercel.app/",
+		githubUrl: "https://github.com/myc37/WookieLeaks",
 		stack: [TECH.NEXTJS, TECH.TAILWIND, TECH.PRISMA],
 	},
 	{
 		name: "partnerUp",
 		blurb: "partnerUp provides a central platform for users browse posts and join exisiting groups which share their interests or create their own posts to find other like-minded individuals!",
 		url: "https://orbital-teamtams-partnerup.herokuapp.com/home",
+		githubUrl: "https://github.com/myc37/NUS_Orbital_2021",
 		stack: [TECH.REACT, TECH.MUI, TECH.FIREBASE, TECH.ALGOLIA],
 	},
 	{
 		name: "financeMeister",
 		blurb: "FinanceMeister allows users to input their transactions and generate useful statistics daily, weekly or monthly period. It also features a kanban board for goal setting as well as a news section which displays recent finance-related articles.",
 		url: "https://financemeister.vercel.app/",
+		githubUrl: "https://github.com/myc37/hacknroll2022",
 		stack: [TECH.REACT, TECH.TAILWIND, TECH.CHARTJS, TECH.FIREBASE],
 	},
 	{
 		name: "PlanPal",
 		blurb: "PlanPal is a simple scheduler app that helps students to track all their classes, deadlines and other events in one single platform.",
 		url: "https://my-planpal.herokuapp.com/",
+		githubUrl: "https://github.com/myc37/PlanPal",
 		stack: [TECH.REACT, TECH.CHAKRA, TECH.PRISMA, TECH.POSTGRESQL],
 	},
 ];
@@ -71,20 +68,43 @@ const ProjectCard: FC<Project & { mirror: boolean }> = ({
 	name,
 	blurb,
 	url,
+	githubUrl,
 	stack,
 	mirror,
 }) => {
 	const mirrorStyle = mirror ? projectStyles.mirror : "";
 
+	const GitHubButton = () => (
+		<a href={githubUrl} target="_blank" rel="noopener noreferrer">
+			<HoverableIcon name={SOCIAL.GITHUB} />
+		</a>
+	);
+
+	const WebsiteButton = () => (
+		<a href={url} target="_blank" rel="noopener noreferrer">
+			<HoverableIcon name={OTHER.WEBSITE} />
+		</a>
+	);
+
 	return (
 		<div className={`${projectStyles.card} ${mirrorStyle}`}>
+			<span className={projectStyles.mobileHeader}>
+				<h1>{name}</h1>
+				<span className={projectStyles.mobileCta}>
+					<GitHubButton />
+					<WebsiteButton />
+				</span>
+			</span>
 			<div className={projectStyles.imageContainer}>
-				<img src={`/images/${name.toLowerCase()}.jpg`} alt={name} />
+				<Image
+					src={`/images/${name.toLowerCase()}.jpg`}
+					alt={name}
+					height={900}
+					width={1600}
+				/>
 				<div className={projectStyles.overlay}>
-					<a href={url} target="_blank" rel="noopener noreferrer">
-						<HoverableIcon name={OTHER.WEBSITE} />
-					</a>
-					<HoverableIcon name={SOCIAL.GITHUB} />
+					<GitHubButton />
+					<WebsiteButton />
 				</div>
 			</div>
 			<div className={`${projectStyles.descriptor} ${mirrorStyle}`}>
