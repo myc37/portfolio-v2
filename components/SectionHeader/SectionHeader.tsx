5e6e04de97f1b07a2c2f486c0ad9e58b9aa85915
myc37 <nvjn37@gmail.com>
2022-08-15 19:56:13 +0800

    feat: add section header component

---
+++ components/SectionHeader/SectionHeader.tsx
@@ -0,0 +1,32 @@
+import HoverableIcon from "components/HoverableIcon";
+import { SECTION, SIZE } from "lib/enums";
+import { FC } from "react";
+
+type Props = {
+	section: SECTION;
+};
+
+const SectionHeader: FC<Props> = ({ section }) => {
+	return (
+		<span className="header">
+			<HoverableIcon
+				name={section}
+				size={SIZE.LARGE}
+				active
+				hideLabel
+				disableFloat
+			/>
+			<h1>{sectionTitles[section]}</h1>
+		</span>
+	);
+};
+
+const sectionTitles: Record<SECTION, string> = {
+	Home: "",
+	About: "About Me",
+	Work: "Work Experiences",
+	Projects: "Personal Projects",
+	Contact: "Get In Touch!",
+};
+
+export default SectionHeader;
