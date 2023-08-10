1b00f28e8cd48e476912cfc8fd9fb10f452721e5
N Vijay Narayanan <nvjn37@gmail.com>
2023-08-11 01:11:13 +0800

    chore: update about me blurb

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -10,16 +10,19 @@ const About = () => {
 				<SectionHeader section={SECTION.ABOUT} />
 				<div className={aboutStyles.content}>
 					<p>
-						I am currently a Year 3 Computer Science undergraduate
-						at the National University of Singapore (NUS), with
-						specializations in Software Engineering as well as
-						Database Systems
+						I am currently a Final Year Computer Science
+						undergraduate at the National University of Singapore
+						(NUS), with specializations in Software Engineering as
+						well as Database Systems.
 					</p>
 					<p>
 						My web development journey started with NUS School of
 						Computing&apos;s Orbital program back in May 2020. Since
-						then, I found a passion for building applications and
-						products that help to improve the lives of others
+						then, I&apos;ve found a passion for building
+						applications and products that help to improve the lives
+						of others. This goal drives me to continuously improve
+						my web development skills so that I can make a stronger
+						contribution to society.
 					</p>
 				</div>
 			</FadeIn>
