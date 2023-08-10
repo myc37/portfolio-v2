0e709b3a83140377e10b240722d5d70013fb3f5f
N Vijay Narayanan <nvjn37@gmail.com>
2023-08-11 01:14:07 +0800

    chore: remove typing animation, update hero blurb

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,30 +2,19 @@ import FadeIn from "components/FadeIn";
 import React, { FC, RefObject, useEffect, useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 import heroStyles from "./Hero.module.scss";
+import { SIZE, SOCIAL } from "lib/enums";
+import HoverableIcon from "components/HoverableIcon";
+import { socialLinks } from "lib/constants";
+import useResponsive from "hooks/useResponsive";
 
 type Props = {
 	aboutRef: RefObject<HTMLDivElement>;
 };
 
 const Hero: FC<Props> = ({ aboutRef }) => {
-	const [introBlurb, setIntroBlurb] = useState("");
-	const introBlurbText =
+	const { isMobile } = useResponsive();
+	const introBlurb =
 		"Nice to meet you! My name is Vijay, and I hope to make a positive impact on the world through building for the web";
-	const delay = 50;
-
-	useEffect(() => {
-		const nextIntroBlurb = introBlurbText.slice(0, introBlurb.length + 1);
-		const timeout = setTimeout(() => {
-			setIntroBlurb(nextIntroBlurb);
-			if (nextIntroBlurb === introBlurbText) {
-				document
-					.getElementsByClassName(heroStyles.blinkingCursor)[0]
-					?.classList.add(heroStyles.inactive);
-			}
-		}, delay * (["!", ","].includes(nextIntroBlurb.charAt(nextIntroBlurb.length - 2)) ? 5 : 1));
-
-		return () => clearTimeout(timeout);
-	}, [introBlurb]);
 
 	const handleScroll = () => {
 		aboutRef.current?.scrollIntoView({
@@ -37,29 +26,56 @@ const Hero: FC<Props> = ({ aboutRef }) => {
 	return (
 		<div className={heroStyles.container}>
 			<div className={heroStyles.content}>
-				<p className={heroStyles.blinkingCursor}>{introBlurb}</p>
-				<FadeIn
-					direction="down"
-					delay={introBlurbText.length * delay + 1000}
-				>
-					<div
-						className={heroStyles.arrowContainer}
-						onClick={handleScroll}
-					>
-						{[...Array(3)].map((num) => (
-							<FaChevronDown
-								key={num}
-								className={heroStyles.arrow}
-							/>
-						))}
+				<FadeIn direction="down">
+					<div>
+						<p className={heroStyles.greeting}>
+							Nice to meet you! My name is
+						</p>
+						<p className={heroStyles.name}>Vijay.</p>
+						<p className={heroStyles.introBlurb}>
+							I hope to make a positive impact on the world
+							through building for the web.
+						</p>
 					</div>
+					<div>
+						<div className={heroStyles.socials}>
+							{Object.values(SOCIAL).map((social) => (
+								<a
+									key={social}
+									href={socialLinks[social]}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<HoverableIcon
+										name={social}
+										size={SIZE.MEDIUM}
+									/>
+								</a>
+							))}
+						</div>
+					</div>
+					{isMobile ? (
+						<div
+							className={heroStyles.arrowContainer}
+							onClick={handleScroll}
+						>
+							{Array.from(Array(3).keys()).map((num) => (
+								<FaChevronDown
+									key={num}
+									className={heroStyles.arrow}
+								/>
+							))}
+						</div>
+					) : (
+						<></>
+					)}
 				</FadeIn>
 			</div>
 			<ul className={heroStyles.dropContainer}>
-				{[...Array(10)].map((num) => (
+				{Array.from(Array(10).keys()).map((num) => (
 					<li key={num}>
 						<div className={heroStyles.cloverSquare}>
-							{[...Array(8)].map((num) => (
+							{Array.from(Array(8).keys()).map((num) => (
 								<div
 									key={num}
 									className={heroStyles.cloverLeaf}
